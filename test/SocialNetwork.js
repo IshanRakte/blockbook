1dc7f3121ab215340e2f3435b8143b1edddb8e61
Ishan <ishan.rakte@gmail.com>
2022-06-17 20:12:48 +0530

    Smart Contract for Create Posts Added

--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -2,17 +2,17 @@ const { assert } = require('chai')
 // const { Item } = require('react-bootstrap/lib/breadcrumb')
 const _deploy_contracts = require('../migrations/2_deploy_contracts')
 
-const SocialNetwork = artifacts.require('./SocialNetwork.sol')
+const SocialNetwork = artifacts.require('./SocialNetwork.sol');
 
 require('chai')
 .use(require('chai-as-promised'))
 .should()
 
-contract('SocialNetwork', (accounts) => {
+contract('SocialNetwork', ([deployer, author, tipper]) => {
     let socialNetwork
 
     before(async() =>{
-        socialNetwork = await SocialNetwork.deployed()
+        socialNetwork = await SocialNetwork.deployed();
     })
 
 describe('deployment', async() => {
@@ -29,4 +29,38 @@ describe('deployment', async() => {
         assert.equal(name, 'Social Network')
     })
 })
+
+describe('posts', async() => {
+    let result, postCount;
+
+    // before(async()=>{
+    //     result = await socialNetwork.createPost('Body of Post',{from: author});
+    //     postCount = await socialNetwork.postCount();
+    // })
+
+    it('creates posts', async() => {
+        result = await socialNetwork.createPost('This is my first post', {from: author})
+        postCount = await socialNetwork.postCount()
+
+        assert.equal(postCount, 1)
+        const event = result.logs[0].args
+        // console.log(event)
+        assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct')
+        assert.equal(event.content, 'This is my first post', 'content is correct')
+        assert.equal(event.tipAmount, '0', 'tip amount is correct')
+        assert.equal(event.author, author, 'author is correct')
+
+        await socialNetwork.createPost('', {from: author}).should.be.rejected;
+    })
+
+    // it('lists posts', async() => {
+
+    // })
+
+    // it('tip posts', async() => {
+
+    // })
 })
+
+})
+
