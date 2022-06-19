38b1f62e71e7d1447fb19e584a88f86df5965706
Ishan <ishan.rakte@gmail.com>
2022-06-20 00:14:34 +0530

    Frontend Setup Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
+import Navbar from './Navbar'
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData(){
+    const web3 = window.web3
+    
+    const accounts = await web3.eth.getAccounts()
+    this.setState({account: accounts[0]})
+  }
+
+  constructor(props){
+    super(props)
+    this.state = {
+      account: ''
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <Navbar account = {this.state.account} />
+        {/* <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
             href="http://www.dappuniversity.com/bootcamp"
@@ -15,7 +51,7 @@ class App extends Component {
           >
             Dapp University
           </a>
-        </nav>
+        </nav> */}
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
