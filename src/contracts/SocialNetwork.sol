56af05f2d854550122d7ac37fb56a782265b9049
Ishan <ishan.rakte@gmail.com>
2022-06-19 20:37:13 +0530

    Smart Contract for Tip Posts Added

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -10,14 +10,21 @@ contract SocialNetwork {
         uint id;
         string content;
         uint tipAmount;
-        address author;
+        address payable author;
     }
 
     event PostCreated(
         uint id,
         string content,
         uint tipAmount,
-        address author
+        address  payable author
+    );
+
+    event PostTipped(
+        uint id,
+        string content,
+        uint tipAmount,
+        address  payable author
     );
 
     constructor() public{
@@ -29,5 +36,18 @@ contract SocialNetwork {
         postCount++;
         posts[postCount] = Post(postCount, _content, 0, msg.sender);
         emit PostCreated(postCount, _content, 0, msg.sender);
+    }
+
+    function tipPost(uint _id) public payable{
+
+        require(_id > 0 && _id <= postCount);
+        Post memory _post = posts[_id];
+        address payable _author = _post.author;
+        address(_author).transfer(msg.value);
+        _post.tipAmount = _post.tipAmount + msg.value;
+
+        posts[_id] = _post;
+
+        emit PostTipped(postCount, _post.content, _post.tipAmount, _author);
     }
 }
